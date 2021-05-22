63aca59634688ac4199f165c55d35dc4bfd8a244
Amit Agrawal <amit.agrawal@hyperverge.co>
2021-05-22 20:10:25 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.f0b0ce46917ec5aa2b56ea7c28f3534e.js"
+  "/portfolio/precache-manifest.60c8cbd07582b4255615ba5e0b478fb8.js"
 );
 
 self.addEventListener('message', (event) => {
