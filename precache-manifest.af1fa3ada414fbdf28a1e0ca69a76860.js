8d45925d23e50ad8711cddb153ad0a1ca9b26fc3
Amit Agrawal <amit.agrawal@hyperverge.co>
2021-05-22 15:12:14 +0530

    Updates

---
+++ precache-manifest.af1fa3ada414fbdf28a1e0ca69a76860.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0725818a5d0c1a3bde67da6682de5c74",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c7a059b9731af59d2ef7",
+    "url": "/portfolio/static/css/2.36b72570.chunk.css"
+  },
+  {
+    "revision": "04c23b518f537762a988",
+    "url": "/portfolio/static/css/main.aef4b29c.chunk.css"
+  },
+  {
+    "revision": "c7a059b9731af59d2ef7",
+    "url": "/portfolio/static/js/2.5c51a3ea.chunk.js"
+  },
+  {
+    "revision": "67103adef8371e97262c4460a4f586be",
+    "url": "/portfolio/static/js/2.5c51a3ea.chunk.js.LICENSE"
+  },
+  {
+    "revision": "04c23b518f537762a988",
+    "url": "/portfolio/static/js/main.aac15427.chunk.js"
+  },
+  {
+    "revision": "0441743606cc8b21f0f3",
+    "url": "/portfolio/static/js/runtime-main.381687a1.js"
+  }
+]);
